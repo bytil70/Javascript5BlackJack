1ec04b4f9543af7f306f7acfcd84d0741a54f46e
bytil70 <bytil70@gmail.com>
2025-03-28 10:40:38 +0000

    updt

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 11
 
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ''
 
-let messageEl = document.getElementById('mssage-el')
-console.log(msgEl)
+let messageEl = document.getElementById('message-el')
+console.log(messageEl)
 
 function startGame() {
 
     if (sum <= 20) {
-        message = 'Do you want to draw a new card? 🤔'
+        message = 'Do you want to draw a new card?'
     } else if (sum === 21) {
-    message ='Wohoo! You have got a Blackjack! 🥳'
-    hasBlackjack = true
+        message ='You have got a Blackjack!'
+        hasBlackjack = true
     } else {
-    message ='You are out of the game! 😭'
-    isAlive = false
+        message ='You are out of the game!'
+        isAlive = false
     }
     messageEl.textContent = message
 }
