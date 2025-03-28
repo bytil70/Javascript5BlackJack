e56756f3b6e5885bdff2f9a607abafdb7c1eea34
bytil70 <bytil70@gmail.com>
2025-03-28 08:00:33 +0000

    start

---
+++ index.js
@@ -0,0 +1,12 @@
+let firstCard = 6
+let secondCard = 9
+
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log('Do you want to draw a new card? 🤔')
+} else if (sum === 21) {
+    console.log('Wohoo! You have got a Blackjack! 🥳')
+} else {
+    console.log('You are out of the game! 😭')
+}
