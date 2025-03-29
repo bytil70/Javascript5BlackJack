8dc5b2e9848c9def53923cdaae47c4e1c961acd3
bytil70 <bytil70@gmail.com>
2025-03-29 06:30:20 +0000

    update

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-let firstCard = 10
-let secondCard = 4
-// create a new array - cards - that contain firstCard and secondCard
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+
+let player = {
+    name: 'BurnLai',
+    chips: 200
+    }
+
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ''
-
 let messageEl = document.getElementById('message-el')
-
 let sumEl = document.getElementById('sum-el')
-
 let cardsEl = document.getElementById('cards-el')
+let playerEl = document.getElementById('player-el')
+
+playerEl.textContent = player.name + ': $' + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else{
+        return randomNumber
+    }
+}
+
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    // general 2 anom nums
+    // Re-assign the cards and sum variables so that the game can start
     renderGame()
 }
 
 function renderGame() {
     // Refer to the cards array when rendering out the cards
-    cardsEl.textContent = 'Cards: ' + cards[0] + ' ' + cards[1]
-    // render out all the cards we have
+    cardsEl.textContent = 'Cards: '
+    // create a for loop that renders out all the cards instead of just two
+    for (let i = 0; i < cards.length; i ++) {
+        cardsEl.textContent += cards[i] + ' '
+    }
     sumEl.textContent = 'Sum: ' + sum
 
     if (sum <= 20) {
@@ -36,11 +61,10 @@ function renderGame() {
 }
 
 function newCard() {
-    console.log('Drawing a new card from the deck!')
-    let card = 6
-    sum += card
-    // push the card to the cards array
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    if (isAlive === true && hasBlackjack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
--- index2.js
+++ index2.js
@@ -41,3 +41,104 @@ console.log(messages)
 messages.pop()
 console.log(messages)
 
+// count in JavaScript
+// star finish step
+for(let count = 1; count < 11; count += 2) {
+    console.log(count)
+}
+
+// create a for loop in JavaScript
+for(let i = 0; i < 6; i += 1) {
+    console.log(i)
+}
+
+// write array base loop
+let cards = [7, 3, 9]
+for (let i = 0; i < cards.length; i += 1) {
+    console.log(cards[i])
+}
+
+//DOM
+let sentence = ['Hello', 'my', 'name', 'is', 'Per']
+let greetingEl = document.getElementById('greeting-el')
+
+for(let i = 0; i < sentence.length; i++) {
+    greetingEl.textContent += sentence[i] + ' ' 
+}
+
+// return value in function
+let player1Time = 102
+let player2Time = 107
+
+function getFasterRaceTime() {
+    if(player1Time > player2Time) {
+        return player1Time
+    } else if (player2Time > player1Time) {
+        return player2Time
+    } else {
+        return player1Time
+    }
+}
+
+function getTotalRaceTime() {
+    return player1Time + player2Time
+}
+
+let totalTime = getFasterRaceTime()
+console.log(totalTime)
+
+//use random in JavScript to create a ie function
+let randomNumber = Math.floor(Math.random() * 6) + 1
+console.log(randomNumber)
+
+function rollDice() {
+    let randomNumber = Math.floor(Math.random() * 6) + 1
+    return randomNumber
+}
+
+console.log(rollDice())
+
+let hasCompletedCourse = false
+let givesCertificate = true
+
+if (hasCompletedCourse === true && givesCertificate === true) {
+    generateCertificate()
+}
+
+if (hasCompletedCourse === true || givesCertificate === true) {
+    generateCertificate()
+}
+
+function generateCertificate() {
+    console.log('Generating certification...')
+}
+
+// Objects -store data in-depth - composite / complex data type
+// key-valve pairs
+
+let course = {
+    title: 'Learn CSS Grid for free',
+    lesson: 16,
+    creator: 'Per Harald Borgen',
+    length: 63,
+    level: 2,
+    isFree: true,
+    tags: ['html', 'css']
+}
+
+console.log(course.tags)
+
+// create your first object
+// create an object that represents an airbnb castle listing
+// it should contain at least one boolean, one string, one numr and one array
+// log out at least two of the keys using the dot notaion
+
+let castle = {
+    title: 'Live like a king in my castle',
+    price: 190,
+    isSuperHost: true,
+    images:['img/castle1.png', 'img/castle2.png']
+}
+
+console.log(castle.price)
+console.log(castle.isSuperHost)
